953a57ae7b14c2393f96231427464efe92c568a8
shigeta0506 <g2227042@st.t-kougei.ac.jp>
2022-10-24 20:12:27 +0900

    テストを通しました

--- src/include/lib_func.h
+++ src/include/lib_func.h
@@ -7,7 +7,7 @@ extern "C" {
 
 
 // 2つの数字が等しいですか？
-bool is_same(int val1, int val2);
+	bool is_same(int val1, int val2);
 
 // val1 から val3 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_3(int sum, int val1, int val2, int val3);
