1b858a712d4665b8af6df7b0ff9b3d1b0bea3ea1
yuhi610 <bird9.yuhi@gmail.com>
2022-10-24 17:36:00 +0900

    関数の修正

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -30,11 +30,33 @@ int calc_combinations_3(int sum, int val1, int val2, int val3)
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4)
 {
-	return 0;
+	int num=0;
+	int combi[15]={val1, val2, val3, val4, 
+	val1+val2, val1+val3, val1+val4, val2+val3, val2+val4, val3+val4,
+	val1+val2+val3, val1+val2+val4, val1+val3+val4, val2+val3+val4,
+	val1+val2+val3+val4};
+
+	for(int i=0;i<15;i++){
+		if(combi[i]==sum){num++;}
+	}
+	return num;
 }
 
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations(int sum, const int* val_array, int array_size)
 {
-	return 0;
+	int num=0;
+	int combi[256]={0};
+	int cbt=0;
+	for(int ary_num=0; ary_num <array_size; ary_num++){
+		
+		combi[cbt++]=val_array[ary_num];
+		if(combi[cbt-1]==sum){num++;}
+		int s=cbt;
+		for(int i=0;i<s-1;i++){
+			combi[cbt++]=val_array[ary_num]+combi[i];
+			if(combi[cbt-1]==sum){num++;}
+		}
+	}
+	return num;
 }
